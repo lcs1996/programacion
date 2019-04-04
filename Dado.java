fd129e4cf921c61bf3544519091a1fe32cda3bea
luis <luis.96colon@gmail.com>
2019-04-04 09:10:10 +0200

    version 1.1 clase Dado.java

--- Dado.java
+++ Dado.java
@@ -1,8 +1,18 @@
+
 public class Dado{
 	private int valor;
 	
+	public Dado () {
+		;
+	}
+	
 	public void tirarDado(){
 		//este metodo dara un valor aleatorio
-		
+		this.valor=(int) ((Math.random()*6)+1);
+		return this.valor;
 	}
+	
+	public int getValor() {
+	return this.valor;
+	}	
 }
